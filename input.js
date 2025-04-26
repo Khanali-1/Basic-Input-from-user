ee39b0cb4f41e9672e225bb68db4e4c0fb49b16d
Habib Khan <meo24693@gmail.com>
2025-04-26 04:30:52 -0700

    add new file

---
+++ input.js
@@ -0,0 +1,14 @@
+// simple and basic input from user
+// for example what is you'r name and whats is ur age ?
+
+// simple input of name from user
+let username=window.prompt(" what is you'r Name ?");
+document.getElementById("name").textContent=username;
+
+// simple input of age from user
+let age=window.prompt("what is you'r Age ?");
+document.getElementById("age").textContent=age;
+
+// simple input of job from user 
+let job=window.prompt("what is uou'r job");
+document.getElementById("job").textContent=job;
